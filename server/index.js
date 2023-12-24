05260eb4a226baaa101b3ed69e6c01d333259c7d
Aditya250401 <aa.21u10091@btech.nitdgp.ac.in>
2023-12-24 10:25:27 +0530

    added the aws pipeline

--- server/index.js
+++ server/index.js
@@ -11,17 +11,17 @@ app.use(express.json())
 
 // Routes
 app.use('/', (req, res) => {
-	res.send('Welcome to the course management system')
+	res.send('i have succesfully implemented the complete pipeline')
 })
 app.use('/admin', adminRouter)
 app.use('/user', userRouter)
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	dbName: 'courses',
-})
+// mongoose.connect('mongodb://localhost:27017/courses', {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	dbName: 'courses',
+// })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
